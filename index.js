65260a5971219c14c0ded169c5ecb9f7bab30803
Ernesto Rodriguez <ernesto.rodriguez@mercadolibre.com>
2019-11-02 17:33:13 -0300

    added express server and api and server enpoints

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const config = require('config');
+const App = express();
+
+const port = config.get('app.port');
+const api = require('./api');
+const server = require('./app/server');
+
+App.use('/api/', api);
+App.use('/', server);
+App.use(express.static('dist'));
+App.listen(port, () => console.log(`Server running on port: ${port}!`));
