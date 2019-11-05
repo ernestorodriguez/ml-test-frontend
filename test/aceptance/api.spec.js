0118cbc05ebaa50acbff03a48b5df1cdae8b9a6a
Ernesto Rodriguez <ernesto.rodriguez@mercadolibre.com>
2019-11-04 21:04:16 -0300

    added cases for full coverage

--- test/aceptance/api.spec.js
+++ test/aceptance/api.spec.js
@@ -36,18 +36,42 @@ describe('Acceptance test for Api items', () => {
             },
             categories: ['Celulares y Teléfonos', 'Celulares y Smartphones'],
             items:[
-               {
-                   id: 'MLA823005561',
-                   title: 'Apple iPhone 6s 32 Gb Gris Espacial',
-                   price: {
+                {
+                    id: 'MLA823005561',
+                    title: 'Apple iPhone 6s 32 Gb Gris Espacial',
+                    price: {
                        currency: '$',
                        amount: '30990',
                        decimals: '20'
-                   },
-                   picture: 'http://mla-s2-p.mlstatic.com/898290-MLA31003118647_062019-I.jpg',
-                   condition: 'new',
-                   free_shipping: true,
-               }
+                    },
+                    picture: 'http://mla-s2-p.mlstatic.com/898290-MLA31003118647_062019-I.jpg',
+                    condition: 'new',
+                    free_shipping: true,
+                },
+                {
+                    id: 'MLA823005563',
+                    title: 'Apple iPhone 7s 32 Gb Gris Espacial',
+                    price: {
+                        currency: '$',
+                        amount: '40990',
+                        decimals: '70'
+                    },
+                    picture: 'http://mla-s2-p.mlstatic.com/898290-MLA31003118647_062019-I.jpg',
+                    condition: 'new',
+                    free_shipping: true,
+                },
+                {
+                    id: 'MLA689528238',
+                    title: 'Funda Silicone Case 5s Se 6 6s 7 Plus iPhone 8 Plus Original',
+                    price: {
+                        currency: '$',
+                        amount: '699',
+                        decimals: '90'
+                    },
+                    picture: 'http://mla-s1-p.mlstatic.com/679417-MLA31019694444_062019-I.jpg',
+                    condition: 'new',
+                    free_shipping: false,
+                }
             ],
        }));
    });
