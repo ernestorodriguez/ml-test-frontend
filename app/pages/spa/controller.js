b15953251c5080f52e265f32236b32916ed995b1
Ernesto Rodriguez <ernesto.rodriguez@mercadolibre.com>
2019-11-18 20:48:00 -0300

    eslint fix

--- app/pages/spa/controller.js
+++ app/pages/spa/controller.js
@@ -1,8 +1,6 @@
 const React = require('react');
 const View = require('./view');
 
-const controller = () => {
-    return  <View/>;
-};
+const controller = () => <View/>;
 
 module.exports = controller;
